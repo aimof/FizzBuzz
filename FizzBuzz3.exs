4a2a94dc8e73e75a2caa5bb1da8fe7d2a6620238
aimof <reception@go-fmt.com>
2016-09-27 16:13:03 +0900

    完成

--- FizzBuzz3.exs
+++ FizzBuzz3.exs
@@ -1,15 +1,10 @@
 defmodule FizzBuzz do
 
   def fizzBuzz(num) do
-    _notFB(_fizz(_check(_ternary(num))) <> _buzz(_check(_quinary(num))), num)
+
   end
 
-  def _ternary(num), do: Integer.to_charlist(num, 3)
-  def _quinary(num), do: Integer.to_charlist(num, 5)
-
-  def _check([48 | []]), do: true
-  def _check([_ | []]), do: false
-  def _check([_ | tail]), do: _check(tail)
+  def str(num), do: Integer.to_charlist(num)
 
   def _fizz(true), do: "Fizz"
   def _fizz(false), do: ""
