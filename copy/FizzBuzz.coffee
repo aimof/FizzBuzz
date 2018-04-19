cc5db4d17867ed4c83e5434bc7bb85f92623ef1d
aimof <aimof.aimof@gmail.com>
2018-04-19 23:35:21 +0900

    clean

--- copy/FizzBuzz.coffee
+++
@@ -1,2 +0,0 @@
-["fizz" unless i%3]+["buzz" unless i%5] or i for i in [1..100]
-# http://c2.com/cgi/wiki?FizzBuzzTest より
--- copy/FizzBuzz5.rb
+++
@@ -1,6 +0,0 @@
-# %（あまり計算）なし
-# http://qiita.com/esehara@github/items/a4fbdc64896663add2ab を参考に自作
-
-buzz = ["0","5"].include?(n.to_s[-1]) ? "Buzz" : ""
-
-#書きかけ
